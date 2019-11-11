4fe756a5a94d3043da0b350f6661aa4b7d462f9a
HarshitChhipa <harshitkankerwal@gmail.com>
2019-11-11 10:06:04 +0530

    Added the main mail id

--- demo.js
+++ demo.js
@@ -1,3 +1,4 @@
 console.log('I a am getting into the inner of git');
 console.log('I am just checking it out man');
 console.log('I am trying to play with reset');
+console.log('Added main mail id to push the changes');

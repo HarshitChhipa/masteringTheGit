ed390a4a6facf0ffa19896d044b322520ec007de
HarshitChhipa <harshitkankerwal@gmail.com>
2019-11-11 10:08:12 +0530

    Added few more things like copying the code

--- demo.js
+++ demo.js
@@ -2,3 +2,7 @@ console.log('I a am getting into the inner of git');
 console.log('I am just checking it out man');
 console.log('I am trying to play with reset');
 console.log('Added main mail id to push the changes');
+console.log('I a am getting into the inner of git');
+console.log('I am just checking it out man');
+console.log('I am trying to play with reset');
+console.log('Added main mail id to push the changes');
